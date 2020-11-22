f2f5f3713f7a8982c1c75c59211f1b90666895b2
RizkyAnggita <rizkysyarbaini@rocketmail.com>
2020-11-22 22:40:22 +0700

    Instruksi dasar OK

---
+++ main.pl
@@ -0,0 +1,34 @@
+:- include('map.pl').
+:- include('character.pl').
+:- include('quest.pl').
+:- include('enemy.pl').
+
+print_start :-
+    write('LET\'S FIND DONATELLO!'),nl,nl,
+    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'),nl,
+    write('%                              ~Find Donatello~                                %'),nl,
+    write('% 1. start  : untuk memulai petualanganmu                                      %'),nl,
+    write('% 2. map    : menampilkan peta                                                 %'),nl,
+    write('% 3. status : menampilkan kondisimu terkini                                    %'),nl,
+    write('% 4. w      : gerak ke utara 1 langkah                                         %'),nl,
+    write('% 5. s      : gerak ke selatan 1 langkah                                       %'),nl,
+    write('% 6. d      : gerak ke ke timur 1 langkah                                      %'),nl,
+    write('% 7. a      : gerak ke barat 1 langkah                                         %'),nl,
+    write('% 9. status : menampilkan status pemain                                        %'),nl,
+    write('% 8. help   : menampilkan segala bantuan                                       %'),nl,
+    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'),nl,nl.
+
+
+
+start :-
+    print_start,
+    initLocPlayer,
+    initProgressQuest,
+    initEnemy,
+    character.
+
+status :-
+    printstats.
+
+help :-
+    print_start.
