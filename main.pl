23ee2f591f9e26248d5c7c5c23944251c979f28d
RizkyAnggita <rizkysyarbaini@rocketmail.com>
2020-11-25 23:53:29 +0700

    add command

--- main.pl
+++ main.pl
@@ -3,20 +3,22 @@
 :- include('quest.pl').
 :- include('inventory.pl').
 :- include('enemy.pl').
+:- include('battle.pl').
 
 print_start :-
     write('LET\'S FIND DONATELLO!'),nl,nl,
     write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'),nl,
-    write('%                              ~Find Donatello~                                %'),nl,
+    write('%                              ~Find Donatello~                                   %'),nl,
     write('% 1. start     : untuk memulai petualanganmu                                      %'),nl,
     write('% 2. map       : menampilkan peta                                                 %'),nl,
     write('% 3. status    : menampilkan status pemain                                        %'),nl,
     write('% 4. w         : gerak ke utara 1 langkah                                         %'),nl,
     write('% 7. a         : gerak ke barat 1 langkah                                         %'),nl,
     write('% 5. s         : gerak ke selatan 1 langkah                                       %'),nl,
-    write('% 6. d         : gerak ke timur 1 langkah                                      %'),nl,
-    write('% 9. inventory : menampilkan inventory                                            %'),nl,
+    write('% 6. d         : gerak ke timur 1 langkah                                         %'),nl,
+    write('% 7. inventory : menampilkan inventory                                            %'),nl,
     write('% 8. help      : menampilkan segala bantuan                                       %'),nl,
+    write('% 9. quest     : menampilkan quest dan progress quest yang dimiliki               %'),nl,
     write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'),nl,nl.
 
 
@@ -31,6 +33,12 @@ start :-
 
 status :-
     printstats.
+
+quest :-
+    print_getQuest.
+
+potion :-
+    usePotion.
 
 help :-
     print_start.
