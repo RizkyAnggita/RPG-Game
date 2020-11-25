392a055eafae8035e38de10a3f460b4803739f6e
ariyaadinatha <ariyaadinatha21@gmail.com>
2020-11-25 18:28:56 +0700

    tambahan item

--- inventory.pl
+++ inventory.pl
@@ -3,34 +3,41 @@
 :- dynamic(inventoryData/7).
 
 
-%decodeItemX(<character_id>,<is_item_exist>,<item_name>)
-decodeItem1(1,1,dagger).
-decodeItem1(2,1,katana).
-decodeItem1(3,1,brass_knuckle).
+%decodeItemX(<character_id>,<is_item_exist>,<item_id>,<item_name>)
+decodeItem1(1,1,1,dagger).
+decodeItem1(2,1,2,katana).
+decodeItem1(3,1,3,brass_knuckle).
 
-decodeItem2(1,1,chakram).
-decodeItem2(2,1,long_sword).
-decodeItem2(3,1,nunchaku).
+decodeItem2(1,1,4,chakram).
+decodeItem2(2,1,5,long_sword).
+decodeItem2(3,1,6,nunchaku).
 
-decodeItem3(1,1,invisible_cloth).
-decodeItem3(2,1,helmet).
-decodeItem3(3,1,hand_bandage).
+decodeItem3(1,1,7,invisible_cloth).
+decodeItem3(2,1,8,helmet).
+decodeItem3(3,1,9,hand_bandage).
 
-decodeItem4(1,1,chain_mail).
-decodeItem4(2,1,karuta).
-decodeItem4(3,1,armor).
+decodeItem4(1,1,10,chain_mail).
+decodeItem4(2,1,11,karuta).
+decodeItem4(3,1,12,armor).
 
-%inventoryData(char_id,item_1, item_2, item_3, item_4, potion_1, potion_2).
+decodeItem4(_,1,13,potion_sm).
+decodeItem4(_,1,14,potion_md).
+decodeItem4(_,1,15,potion_xl).
+decodeItem4(_,1,16,hammer_of_thor).
+decodeItem4(_,1,17,dulcolax).
+decodeItem4(_,1,18,temulawak).
+
+%inventoryData(char_id,item_1, item_2, item_3, item_4, potion_1, potion_2, potion_3, potion_4, potion_5, potion_6).
 initInventory :-
-    asserta(inventoryData(1,1,0,0,0,5,0)),
-    asserta(inventoryData(2,1,0,0,0,5,0)),
-    asserta(inventoryData(3,1,0,0,0,5,0)).
+    asserta(inventoryData(1,1,0,0,0,5,0,0,0,0,0)),
+    asserta(inventoryData(2,1,0,0,0,5,0,0,0,0,0)),
+    asserta(inventoryData(3,1,0,0,0,5,0,0,0,0,0)).
 
 printInventory :-
     user(CHARACTER,CLASS,_,_,_,_,_,_), 
     getid(CHARACTER,Char_id),
     format('Inventory of ~w (~w):~n', [CHARACTER, CLASS]),
-    inventoryData(Char_id,Num_item_1, Num_item_2, Num_item_3, Num_item_4, Num_potion_1, Num_potion_2),
+    inventoryData(Char_id,Num_item_1, Num_item_2, Num_item_3, Num_item_4, Num_potion_1, Num_potion_2, Num_potion_3, Num_potion_4, Num_potion_5, Num_potion_6),
     decodeItem1(Char_id,1,Item_1),
     decodeItem2(Char_id,1,Item_2),
     decodeItem3(Char_id,1,Item_3),
