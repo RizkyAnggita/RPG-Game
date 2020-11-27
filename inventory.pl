828878a2a5a76c0d96a6f7b07bad4f75ffd49be1
RizkyAnggita <rizkysyarbaini@rocketmail.com>
2020-11-27 14:22:25 +0700

    BONUS Potion OK

--- inventory.pl
+++ inventory.pl
@@ -1,7 +1,4 @@
-:- include('character.pl').
-
 :- dynamic(inventoryData/11).
-
 
 %decodeItemX(<character_id>,<is_item_exist>,<item_id>,<item_name>)
 decodeItem1(1,1,1,dagger).
@@ -20,11 +17,6 @@ decodeItem4(1,1,4,chain_mail).
 decodeItem4(2,1,4,karuta).
 decodeItem4(3,1,4,armor).
 
-
-
-
-
-
 decodePotion1(any,1,13,potion_sm).
 decodePotion2(any,1,14,potion_md).
 decodePotion3(any,1,15,potion_xl).
@@ -32,18 +24,19 @@ decodePotion4(any,1,16,hammer_of_thor).
 decodePotion5(any,1,17,dulcolax).
 decodePotion6(any,1,18,temulawak).
 
-potionPlusHP(13,30).
-potionPlusHP(14,50).
-potionPlusHP(15,75).
-potionPlusHP(16,100).
-potionPlusHP(17,150).
-potionPlusHP(18,200).
+/* potionPlus(Id,Plus) Plus berisi pertambahan, bisa HP, Attack, Defense,MaxHP */
+potionPlus(13,30).
+potionPlus(14,75).
+potionPlus(15,125).
+potionPlus(16,100).
+potionPlus(17,100).
+potionPlus(18,100).
 
 %inventoryData(char_id,item_1, item_2, item_3, item_4, potion_1, potion_2, potion_3, potion_4, potion_5, potion_6).
 initInventory :-
     user(CHARACTER,CLASS,_,_,_,_,_,_,_), 
     getid(CHARACTER,Char_id),
-    asserta(inventoryData(Char_id,1,0,0,0,5,0,0,0,0,0)).
+    asserta(inventoryData(Char_id,1,0,0,0,5,0,0,1,1,1)).
 
 isInventoryNotFull:-
     user(CHARACTER,CLASS,_,_,_,_,_,_,_), 
@@ -79,32 +72,35 @@ usePotion :-
     repeat, 
         read(Input),
     (Input=:=13; Input=:=14; Input=:=15; Input=:=16; Input=:=17; Input=:=18; Input=:=99),
-    kuranginPotion(Input),
-    (Input =:= 99 -> (!,fail); (1=:=1)),
-    potionPlusHP(Input, PlusHP),
-    user(Char, Class, UserCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP),
-    A is UserCurrHP + PlusHP,
-    (A > UserHP -> NewCurrHP is UserHP ; NewCurrHP is A),
-
-    
-
-    retract(user(Char, Class, UserCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP)),
-    asserta(user(Char, Class, NewCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP)),!.
+    effectPotion(Input),
+    (Input =:= 99 -> (!,fail); (1=:=1)), !.
 
 
-kuranginPotion(Id) :-
+effectPotion(Id) :-
     inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,POTION3,POTION4,POTION5,POTION6),
+    user(Char, Class, UserCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP),
+    potionPlus(Id, Plus),
     (Id =:= 13 -> 
-        (   POTION1 > 0,
+        (   
+            A is UserCurrHP + Plus,
+            (A > UserHP -> NewCurrHP is UserHP ; NewCurrHP is A),
+            POTION1 > 0,
             NEWPOTION1 is POTION1 - 1,
+            retract(user(Char, Class, UserCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP)),
+            asserta(user(Char, Class, NewCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP)),
             retract(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,POTION3,POTION4,POTION5,POTION6)),
             asserta(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,NEWPOTION1,POTION2,POTION3,POTION4,POTION5,POTION6))
         ); (1 =:= 1)
     ),    
     (Id =:= 14 
     -> 
-        (   POTION2 > 0,
+        (   
+            A is UserCurrHP + Plus,
+            (A > UserHP -> NewCurrHP is UserHP ; NewCurrHP is A),
+            POTION2 > 0,
             NEWPOTION2 is POTION2 - 1,
+            retract(user(Char, Class, UserCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP)),
+            asserta(user(Char, Class, NewCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP)),
             retract(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,POTION3,POTION4,POTION5,POTION6)),
             asserta(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,NEWPOTION2,POTION3,POTION4,POTION5,POTION6))
         )
@@ -112,17 +108,26 @@ kuranginPotion(Id) :-
     ),
     (Id =:= 15 
     -> 
-        (   POTION3 > 0,
+        (   
+            A is UserCurrHP + Plus,
+            (A > UserHP -> NewCurrHP is UserHP ; NewCurrHP is A),
+            POTION3 > 0,
             NEWPOTION3 is POTION3 - 1,
+            retract(user(Char, Class, UserCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP)),
+            asserta(user(Char, Class, NewCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP)),
             retract(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,POTION3,POTION4,POTION5,POTION6)),
-                    asserta(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,NEWPOTION3,POTION4,POTION5,POTION6))
+            asserta(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,NEWPOTION3,POTION4,POTION5,POTION6))
         )
         ; (1 =:= 1)
     ),
     (Id =:= 16 
     -> 
-        (   POTION4 > 0,
+        (   
+            NEWATTACK is UserAtt + 100,
+            POTION4 > 0,
             NEWPOTION4 is POTION4 - 1,
+            retract(user(Char, Class, UserCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP)),
+            asserta(user(Char, Class, UserCurrHP, UserHP, NEWATTACK, UserDef, Gold, Lvl, XP)),
             retract(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,POTION3,POTION4,POTION5,POTION6)),
             asserta(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,POTION3,NEWPOTION4,POTION5,POTION6))
         )
@@ -130,8 +135,12 @@ kuranginPotion(Id) :-
     ),
     (Id =:= 17 
     -> 
-        (   POTION5 > 0,
+        (   
+            NEWDEFENSE is UserDef+100,
+            POTION5 > 0,
             NEWPOTION5 is POTION5 - 1,
+            retract(user(Char, Class, UserCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP)),
+            asserta(user(Char, Class, UserCurrHP, UserHP, UserAtt, NEWDEFENSE, Gold, Lvl, XP)),
             retract(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,POTION3,POTION4,POTION5,POTION6)),
                     asserta(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,POTION3,POTION4,NEWPOTION5,POTION6))
         )
@@ -139,8 +148,12 @@ kuranginPotion(Id) :-
     ),
     (Id =:= 18 
     -> 
-        (   POTION6 > 0,
+        (   
+            NEWMAXHP is UserHP+100,
+            POTION6 > 0,
             NEWPOTION6 is POTION6 - 1,
+            retract(user(Char, Class, UserCurrHP, UserHP, UserAtt, UserDef, Gold, Lvl, XP)),
+            asserta(user(Char, Class, UserCurrHP, NEWMAXHP, UserAtt, UserDef, Gold, Lvl, XP)),
             retract(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,POTION3,POTION4,POTION5,POTION6)),
             asserta(inventoryData(ID,ITEM1,ITEM2,ITEM3,ITEM4,POTION1,POTION2,POTION3,POTION4,POTION5,NEWPOTION6))
         )
@@ -164,19 +177,19 @@ printPotion:-
     decodePotion4(_,_,IdPotion4,NamaPotion4),
     decodePotion5(_,_,IdPotion5,NamaPotion5),
     decodePotion6(_,_,IdPotion6,NamaPotion6),
-    potionPlusHP(IdPotion1, PlusHP1),
-    potionPlusHP(IdPotion2, PlusHP2),
-    potionPlusHP(IdPotion3, PlusHP3),
-    potionPlusHP(IdPotion4, PlusHP4),
-    potionPlusHP(IdPotion5, PlusHP5),
-    potionPlusHP(IdPotion6, PlusHP6),
-    write('Id  Jumlah    Nama Potion         Efek +HP      '),nl,
-    print(IdPotion1), write('    '), print(POTION1), write('        '), print(NamaPotion1), write('             '), print(PlusHP1),nl,
-    print(IdPotion2), write('    '), print(POTION2), write('        '), print(NamaPotion2), write('             '), print(PlusHP2),nl,
-    print(IdPotion3), write('    '), print(POTION3), write('        '), print(NamaPotion3), write('             '), print(PlusHP3),nl,
-    print(IdPotion4), write('    '), print(POTION4), write('        '), print(NamaPotion4), write('         '), print(PlusHP4),nl,
-    print(IdPotion5), write('    '), print(POTION5), write('        '), print(NamaPotion5), write('             '), print(PlusHP5),nl,
-    print(IdPotion6), write('    '), print(POTION6), write('        '), print(NamaPotion6), write('             '), print(PlusHP6),nl,
+    potionPlus(IdPotion1, PlusHP1),
+    potionPlus(IdPotion2, PlusHP2),
+    potionPlus(IdPotion3, PlusHP3),
+    potionPlus(IdPotion4, PlusHP4),
+    potionPlus(IdPotion5, PlusHP5),
+    potionPlus(IdPotion6, PlusHP6),
+    write('Id  Jumlah    Nama Potion             Efek      '),nl,
+    print(IdPotion1), write('    '), print(POTION1), write('        '), print(NamaPotion1), write('            +'), print(PlusHP1), write(' HP'), nl,
+    print(IdPotion2), write('    '), print(POTION2), write('        '), print(NamaPotion2), write('            +'), print(PlusHP2), write(' HP'), nl,
+    print(IdPotion3), write('    '), print(POTION3), write('        '), print(NamaPotion3), write('            +'), print(PlusHP3), write(' HP'),nl,
+    print(IdPotion4), write('    '), print(POTION4), write('        '), print(NamaPotion4), write('       +'), print(PlusHP4) ,write(' Attack'), nl,
+    print(IdPotion5), write('    '), print(POTION5), write('        '), print(NamaPotion5), write('             +'), print(PlusHP5), write(' Defence'),nl,
+    print(IdPotion6), write('    '), print(POTION6), write('        '), print(NamaPotion6), write('            +'), print(PlusHP6),write(' Max HP'),nl,
     write('99    Cancel'),nl.
 
 
